381a384242b483a50db8c655bd2c4e1fd0f7de54
Brandon Wheatley <brandon.a.wheatley@gmail.com>
2020-10-28 09:21:21 -0500

    commiting for new main

--- inVision/client/src/Components/Dream/DreamDetails.js
+++ inVision/client/src/Components/Dream/DreamDetails.js
@@ -1,18 +1,21 @@
 import React, { useEffect, useContext, useState } from "react";
 import { DreamContext } from '../../Providers/DreamProvider';
-import { HowContext} from '../../Providers/HowProvider';
-import {CompletedHowContext} from '../../Providers/CompletedHowProvider';
-import { WhyContext} from '../../Providers/WhyProvider';
+import { HowContext } from '../../Providers/HowProvider';
+import { CompletedHowContext } from '../../Providers/CompletedHowProvider';
+import { WhyContext } from '../../Providers/WhyProvider';
 import { useHistory, useParams, Link } from 'react-router-dom';
 
 export default function DreamDetails() {
-const [dream, setDream] = useState();
-const {getDream} = useContext(DreamContext);
-const { id } = useParams();
+    const [dream, setDream] = useState();
+    const [hows, setHows] = useState([]);
+    const [completedHows, setCompletedHows] = useState([]);
+    const [whys, setWhys] = useState([]);
+    const { getDream } = useContext(DreamContext);
+    const { id } = useParams();
 
-useEffect(() => {
-    getDream(id)
-    .then(setDream);
-},[])
+    useEffect(() => {
+        getDream(id)
+            .then(setDream);
+    }, [])
 
 }
