28e7020d457354e383de53d8d6817afdd557dbf8
Brandon Wheatley <brandon.a.wheatley@gmail.com>
2020-10-28 09:14:31 -0500

    skeleton dream display

---
+++ inVision/client/src/Components/Dream/DreamDetails.js
@@ -0,0 +1,18 @@
+import React, { useEffect, useContext, useState } from "react";
+import { DreamContext } from '../../Providers/DreamProvider';
+import { HowContext} from '../../Providers/HowProvider';
+import {CompletedHowContext} from '../../Providers/CompletedHowProvider';
+import { WhyContext} from '../../Providers/WhyProvider';
+import { useHistory, useParams, Link } from 'react-router-dom';
+
+export default function DreamDetails() {
+const [dream, setDream] = useState();
+const {getDream} = useContext(DreamContext);
+const { id } = useParams();
+
+useEffect(() => {
+    getDream(id)
+    .then(setDream);
+},[])
+
+}
