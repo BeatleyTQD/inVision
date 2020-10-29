3199e0900fc208fae84e95c2870e88b9143fc9e3
Brandon Wheatley <brandon.a.wheatley@gmail.com>
2020-10-29 11:57:55 -0500

    Why delete works

--- inVision/client/src/Components/Why/Why.js
+++ inVision/client/src/Components/Why/Why.js
@@ -1,22 +1,40 @@
-import React from "react";
-import { Button } from 'reactstrap';
+import React, { useContext, useState } from "react";
+import { Button, Modal, ModalHeader, ModalFooter } from 'reactstrap';
 import { useHistory } from "react-router-dom";
+import { WhyContext } from '../../Providers/WhyProvider';
 
 export default function Why({ why }) {
+    const { deleteWhy, getDreamWhys } = useContext(WhyContext);
     const history = useHistory();
+    const [modal, setModal] = useState(false);
+    const toggle = () => setModal(!modal);
 
     const Edit = () => {
         history.push(`/why/edit/${why.id}`)
+    }
+
+    const Delete = () => {
+        deleteWhy(why.id)
+            .then(toggle)
+            .then(getDreamWhys(why.dreamId));
     }
 
     return (
         <>
             <div>
                 {why.description}
-                <Button color="link" onClick={Edit}>Edit</Button>
-                <Button color="link">Delete</Button>
             </div>
-            <br />
+            <div>
+                <Button color="link" onClick={Edit}>Edit</Button>
+                <Button color="link" onClick={toggle}>Delete</Button>{" "}
+                <Modal isOpen={modal} toggle={toggle}>
+                    <ModalHeader toggle={toggle}>Are you sure you want to delete?</ModalHeader>
+                    <ModalFooter>
+                        <Button color="danger" onClick={Delete}>Delete</Button>{' '}
+                    </ModalFooter>
+                </Modal>
+            </div>
+
         </>
     )
 }
